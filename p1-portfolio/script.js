b4affb3e709727675f7ab429dafeb5a0d7ae9938
Jeevan Joseph <jeevanjoseph1310@gmail.com>
2023-01-26 09:05:48 +0530

    added a portfolio website created by chatgpt

---
+++ p1-portfolio/script.js
@@ -0,0 +1,12 @@
+/* script.js */
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+    });
